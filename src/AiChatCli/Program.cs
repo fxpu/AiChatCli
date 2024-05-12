bdcc5c2119f41c9f3ff8cc383b3ab9be3cb72ba6
fxpu <103020143+fxpu@users.noreply.github.com>
2024-05-12 22:41:09 +0200

    Setup initial project structure

---
+++ src/AiChatCli/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FxPu.AiChat.Cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
